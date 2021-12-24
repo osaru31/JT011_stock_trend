6353e10bd7e95a11476d70bd93f33c1620e83616
osaru31 <craftctokaddy@gmail.com>
2021-12-24 10:27:37 +0900

    first commit

---
+++ gather_data.py
@@ -0,0 +1,9 @@
+import sys
+from yahoo_finance_api2 import share
+from yahoo_finance_api2.exceptions import YahooFinanceError
+import pandas as pd
+
+
+class histroical_data:
+    def __init__(self, code) -> None:
+        pass
