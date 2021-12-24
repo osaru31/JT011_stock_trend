70c05ede91d528a01ac53df7d9109aa273f2dabc
osaru31 <craftctokaddy@gmail.com>
2021-12-24 10:28:29 +0900

    delete file

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from yahoo_finance_api2 import share
-from yahoo_finance_api2.exceptions import YahooFinanceError
-import pandas as pd
-
-
-class histroical_data:
-    def __init__(self, code) -> None:
-        pass
